43c30866395d8b8c37f85c8ce7b07fa0912ebad2
Aleksander Popek <aleksander.popek@altermind.pl>
2020-12-03 12:50:34 +0100

    Forced #error replaced with #warning

--- DarkWeather/AppSecrets.swift
+++ DarkWeather/AppSecrets.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 class Secrets {
-    #error("Please add your darksky.net API key below, then delete this line.")
+    #warning("Please add your darksky.net API key below, then delete this line.")
     static var apiKey: String = ""
 }
